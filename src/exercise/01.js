a56c15342770970e6f38f9333ce46d2129f080d4
GersonEC <gersonenriquez94@gmail.com>
2022-03-29 18:27:54 +0200

    implement useReducer extra-credit-2

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,15 +3,19 @@
 
 import * as React from 'react'
 
-const countReducer = (state, step) => {
-  const newState = state + step
+const countReducer = (state, action) => {
+  const newState = {
+    count: action.count,
+  }
   return newState
 }
 
 function Counter({initialCount = 0, step = 2}) {
-  const [count, changeCount] = React.useReducer(countReducer, initialCount)
-
-  const increment = () => changeCount(step)
+  const [state, setState] = React.useReducer(countReducer, {
+    count: initialCount,
+  })
+  const {count} = state
+  const increment = () => setState({count: count + step})
   return <button onClick={increment}>{count}</button>
 }
 
